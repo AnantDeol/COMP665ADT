81b9b4c30f3429c79915b4ab02dec394efcfd8ec
Anant Bir Singh <62200539+AnantDeol@users.noreply.github.com>
2023-12-04 17:22:27 -0500

    Update SAP.py

--- SAP.py
+++ SAP.py
@@ -7,9 +7,8 @@ class SAPDijkstra:
         pass
 
     def sap_dijkstra(self, source, sink, edges, vertices):
-
-        distances = {vertex: float('inf') for vertex in vertices}
-        predecessors = {vertex: None for vertex in vertices}
+        distances = dict((vertex, float('inf')) for vertex in vertices)
+        predecessors = dict((vertex, None) for vertex in vertices)
         distances[source] = 0
 
         # Initialize the priority queue (Q) with starting vertices and distances
@@ -26,12 +25,12 @@ class SAPDijkstra:
         
         augmenting_paths = self.reconstruct_path(predecessors, sink)  
     
-        print(f"Augmenting Path: {augmenting_paths}")
+        print("Augmenting Path: {}".format(augmenting_paths))
         return augmenting_paths
     
     def dfs_like_dijkstra(self, source, sink, edges, vertices):
-        distances = {vertex: float('inf') for vertex in vertices}
-        predecessors = {vertex: None for vertex in vertices}
+        distances = dict((vertex, float('inf')) for vertex in vertices)
+        predecessors = dict((vertex, None) for vertex in vertices)
         counter = 1e9  # Start with a large counter value
 
         distances[source] = 0
@@ -41,27 +40,23 @@ class SAPDijkstra:
         heapq.heapify(Q)
 
         while Q:
-   
             current_distance, current_vertex = heapq.heappop(Q)
-        
             neighbors = [n[1] for n in edges if n[0] == current_vertex and n[2] > 0]
             for neighbor in neighbors:
                 if distances[neighbor] == float('inf'):
                     counter -= 1  # Decrease the key value for v from a large value to a decreasing counter value
                     distances[neighbor] = counter             
                     predecessors[neighbor] = current_vertex
-                    #print(f"Predecessor of {neighbor} is {current_vertex}")
                     heapq.heappush(Q, (distances[neighbor], neighbor))     
 
         augmenting_paths = self.reconstruct_path(predecessors, sink)
 
-        #print(f"Augmenting Path: {augmenting_paths}")
-        #print(f"Visited: {visited}")
+        print("Augmenting Path: {}".format(augmenting_paths))
         return augmenting_paths
     
     def random_dijkstra(self, source, sink, edges, vertices):
-        distances = {vertex: float('inf') for vertex in vertices}
-        predecessors = {vertex: None for vertex in vertices}
+        distances = dict((vertex, float('inf')) for vertex in vertices)
+        predecessors = dict((vertex, None) for vertex in vertices)
 
         distances[source] = 0
 
@@ -70,31 +65,24 @@ class SAPDijkstra:
         heapq.heapify(Q)
 
         while Q:
-   
             current_distance, current_vertex = heapq.heappop(Q)
-        
             neighbors = [n[1] for n in edges if n[0] == current_vertex and n[2] > 0]
             for neighbor in neighbors:
                 if distances[neighbor] == float('inf'):
                     distances[neighbor] = np.random.randint(1, 1000000)            
                     predecessors[neighbor] = current_vertex
-                    #print(f"Predecessor of {neighbor} is {current_vertex}")
                     heapq.heappush(Q, (distances[neighbor], neighbor))     
 
         augmenting_paths = self.reconstruct_path(predecessors, sink)
 
-        print(f"Augmenting Path: {augmenting_paths}")
-        #print(f"Visited: {visited}")
+        print("Augmenting Path: {}".format(augmenting_paths))
         return augmenting_paths
 
 
     def reconstruct_path(self, predecessors, sink):
         path = []
         current = sink
-        #print("Started reconstruction")
         while current is not None:
-            #print(f"current: {current}")
-            path.insert(0, current) # Tracing from sink to back, adding new element on left of list
+            path.insert(0, current)  # Tracing from sink to back, adding a new element on the left of the list
             current = predecessors[current]
-            #print(f"Predecessor: {current}")
         return [path]
