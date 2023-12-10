d709fb9a7c4da8998a5d610a350fdc503a8cd2c7
Anant Bir Singh <62200539+AnantDeol@users.noreply.github.com>
2023-12-10 15:01:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,6 @@ def main(n, r, upper_cap, file_path):
 
     mean_length = total_length/paths if paths > 0 else 0
 
-    print(f"Paths: {paths}")
     print("Paths: {}".format(paths))
     print("Mean Length: {}".format(mean_length))
     print("Mean Proportional Length: {}".format(mean_length/max_length if max_length > 0 else 0))
