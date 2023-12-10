c1940c82d83b51f309ea6af7d3bbfdd0733ffdb3
Anant Bir Singh <62200539+AnantDeol@users.noreply.github.com>
2023-12-10 03:23:39 -0500

    Update dijkstra_variations.py

--- dijkstra_variations.py
+++ dijkstra_variations.py
@@ -2,12 +2,12 @@ from collections import deque
 import heapq
 import random
 
+
 class Dijkstra:
     def __init__(self):
         pass
 
     def sap_dijkstra(self, source, sink, residual_graph, vertices):
-
         distances = {vertex: float('inf') for vertex in vertices}
         predecessors = {vertex: None for vertex in vertices}
         distances[source] = 0
@@ -19,17 +19,17 @@ class Dijkstra:
             current_vertex, current_distance = Q.popleft()
             neighbors = [v for v, capacity in residual_graph[current_vertex] if capacity > 0]
             for neighbor in neighbors:
-                if distances[neighbor] > current_distance + 1: # Taking edge lengths as 1
+                if distances[neighbor] > current_distance + 1:  # Taking edge lengths as 1
                     distances[neighbor] = current_distance + 1
                     predecessors[neighbor] = current_vertex
                     Q.append((neighbor, distances[neighbor]))
-        
-        augmenting_paths = self.reconstruct_path(predecessors, sink)  
-    
+
+        augmenting_paths = self.reconstruct_path(predecessors, sink)
+
         for i in augmenting_paths:
-            print (i)
+            print i
         return augmenting_paths
-    
+
     def dfs_like_dijkstra(self, source, sink, residual_graph, vertices):
         distances = {vertex: float('inf') for vertex in vertices}
         predecessors = {vertex: None for vertex in vertices}
@@ -42,24 +42,23 @@ class Dijkstra:
         heapq.heapify(Q)
 
         while Q:
-   
             current_distance, current_vertex = heapq.heappop(Q)
-        
+
             neighbors = [v for v, capacity in residual_graph[current_vertex] if capacity > 0]
             for neighbor in neighbors:
                 if distances[neighbor] == float('inf'):
                     counter -= 1  # Decrease the key value for v from a large value to a decreasing counter value
-                    distances[neighbor] = counter          
+                    distances[neighbor] = counter
                     predecessors[neighbor] = current_vertex
-                    #print(f"Predecessor of {neighbor} is {current_vertex}")
-                    heapq.heappush(Q, (distances[neighbor], neighbor))     
+                    # print(f"Predecessor of {neighbor} is {current_vertex}")
+                    heapq.heappush(Q, (distances[neighbor], neighbor))
 
         augmenting_paths = self.reconstruct_path(predecessors, sink)
 
-        #print(f"Augmenting Path: {augmenting_paths}")
-        #print(f"Visited: {visited}")
+        # print(f"Augmenting Path: {augmenting_paths}")
+        # print(f"Visited: {visited}")
         return augmenting_paths
-    
+
     def maxcap_dijkstra(self, source, sink, residual_graph, vertices):
         distances = {vertex: float('-inf') for vertex in vertices}
         predecessors = {vertex: None for vertex in vertices}
@@ -77,14 +76,14 @@ class Dijkstra:
                     distances[neighbor] = min(distances[current_vertex], capacity)
                     predecessors[neighbor] = current_vertex
                     Q.append((neighbor, distances[neighbor]))
-        
+
         augmenting_paths = self.reconstruct_path(predecessors, sink)
         max_capacity = distances[sink]
-        
-        #print(f"Augmenting Path: {augmenting_paths}")
-        #print(f"Max Cap: {max_capacity}")
+
+        # print(f"Augmenting Path: {augmenting_paths}")
+        # print(f"Max Cap: {max_capacity}")
         return augmenting_paths
-    
+
     def random_dijkstra(self, source, sink, residual_graph, vertices):
         distances = {vertex: float('inf') for vertex in vertices}
         predecessors = {vertex: None for vertex in vertices}
@@ -96,31 +95,29 @@ class Dijkstra:
         heapq.heapify(Q)
 
         while Q:
-   
             current_distance, current_vertex = heapq.heappop(Q)
-        
+
             neighbors = [v for v, capacity in residual_graph[current_vertex] if capacity > 0]
             for neighbor in neighbors:
                 if distances[neighbor] == float('inf'):
-                    distances[neighbor] = random.randint(1, 1000000)            
+                    distances[neighbor] = random.randint(1, 1000000)
                     predecessors[neighbor] = current_vertex
-                    #print(f"Predecessor of {neighbor} is {current_vertex}")
-                    heapq.heappush(Q, (distances[neighbor], neighbor))     
+                    # print(f"Predecessor of {neighbor} is {current_vertex}")
+                    heapq.heappush(Q, (distances[neighbor], neighbor))
 
         augmenting_paths = self.reconstruct_path(predecessors, sink)
 
-        #print(f"Augmenting Path: {augmenting_paths}")
-        #print(f"Visited: {visited}")
+        # print(f"Augmenting Path: {augmenting_paths}")
+        # print(f"Visited: {visited}")
         return augmenting_paths
-
 
     def reconstruct_path(self, predecessors, sink):
         path = []
         current = sink
-        #print("Started reconstruction")
+        # print("Started reconstruction")
         while current is not None:
-            #print(f"current: {current}")
-            path.insert(0, current) # Tracing from sink to back, adding new element on left of list
+            # print(f"current: {current}")
+            path.insert(0, current)  # Tracing from sink to back, adding new element on left of list
             current = predecessors[current]
-            #print(f"Predecessor: {current}")
+            # print(f"Predecessor: {current}")
         return [path]
