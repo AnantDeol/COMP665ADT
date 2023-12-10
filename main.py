a76c9e4850f4e1ec389b3a3ee1059edbb3cfe748
Anant Bir Singh <62200539+AnantDeol@users.noreply.github.com>
2023-12-10 03:22:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from __future__ import division  # For enabling true division in Python 2
+
 import source_sink_generator
 import ford_fulkerson
 import os
 import csv
-os.chdir(r"C:\Users\anant\OneDrive\Documents\MEngg\COMP 6651 Algo\Project")
+
+#os.chdir(r"C:\Users\anant\OneDrive\Documents\MEngg\COMP 6651 Algo\Project")
 
 
 def main(n, r, upper_cap, file_path):
@@ -18,7 +21,7 @@ def main(n, r, upper_cap, file_path):
             end_index = headers.index("EndNode")
             capacity_index = headers.index("Capacity")
 
-            edges= []
+            edges = []
             for row in reader:
                 start_node = int(row[start_index])
                 end_node = int(row[end_index])
@@ -28,30 +31,29 @@ def main(n, r, upper_cap, file_path):
 
         GG = source_sink_generator.GraphGenerator()
         source = 229
-        #sink = 168
         longest_path = GG.bfs_longest_path(source, edges)
 
         # Define the sink node as the last element of the longest path
         sink = longest_path[-1]
-    
+
     elif opt == "g":
 
         graph_generator = source_sink_generator.GraphGenerator()
         source, sink, edges, vertices = graph_generator.generate_sink_source_graph(n, r, upper_cap)
-    
-    print(f"Source: {source}, Sink: {sink}")
 
-    #Run Ford Fulkerson on the graph
+    print "Source: {}, Sink: {}".format(source, sink)
+
+    # Run Ford Fulkerson on the graph
     FF = ford_fulkerson.FordFulkerson()
 
     residual_graph, paths, total_length, max_length, no_of_edges = FF.ford_fulkerson(source, sink, edges, 3)
 
-    mean_length = total_length/paths if paths > 0 else 0
+    mean_length = total_length / paths if paths > 0 else 0
 
-    print(f"Paths: {paths}")
-    print(f"Mean Length: {mean_length}")
-    print(f"Mean Proportional Length: {mean_length/max_length if max_length > 0 else 0}")
-    print(f"Total Edges: {no_of_edges}")
+    print "Paths: {}".format(paths)
+    print "Mean Length: {}".format(mean_length)
+    print "Mean Proportional Length: {}".format(mean_length / max_length if max_length > 0 else 0)
+    print "Total Edges: {}".format(no_of_edges)
 
 
 if __name__ == "__main__":
